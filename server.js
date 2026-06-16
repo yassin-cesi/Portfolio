1d31596b7cb9f1082f1bd28fbeff604ad3601235
Waazux <yassinriahi03@gmail.com>
2026-06-16 20:29:23 +0200

    fix: add production domain to CORS

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(
   }),
 );
 
-app.use(
-  cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+
+app.use(cors({
+  origin: ["http://localhost:5500", "http://127.0.0.1:5500", "https://yassinriahi.fr"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 app.use(hpp());
 app.use(express.json());
