4017a66050516160c4a8b4dc583b9d0e1da37e15
Waazux <yassinriahi03@gmail.com>
2026-06-16 15:31:05 +0200

    fix database config with MYSQL variables

--- config/database.js
+++ config/database.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 // Création du pool de connexion
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST || process.env.DB_HOST,
-  user: process.env.MYSQL_USER || process.env.DB_USER,
-  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQL_DATABASE || process.env.DB_NAME,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || 3306,
 });
 
 module.exports = db.promise();
