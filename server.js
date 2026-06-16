ea349da62850ffb9f256ff6e2c1d0772bb5e46c2
Waazux <yassinriahi03@gmail.com>
2026-06-16 15:08:23 +0200

    fix PORT for Railway

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use("/images", express.static(path.join(__dirname, "./app/public/images")));
 
 app.use("/api/messages", require("./app/routes/MessageRoutes"));
 
-app.listen(3000, () =>
-  console.log("Serveur connecté sur le port 3000 : http://localhost:3000/api/"),
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Serveur connecté sur le port ${process.env.PORT || 3000}`)
 );
