4e62eafea414b91e21da0860674cae4763cab905
Waazux <yassinriahi03@gmail.com>
2026-06-16 15:23:19 +0200

    update database config for Railway MySQL

--- config/database.js
+++ config/database.js
@@ -2,13 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // Création du pool de connexion
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST || process.env.DB_HOST,
+  user: process.env.MYSQL_USER || process.env.DB_USER,
+  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DATABASE || process.env.DB_NAME,
 });
 
-module.exports = pool.promise();
+module.exports = db.promise();
