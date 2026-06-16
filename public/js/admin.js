849dceb2d33b249aa7f9f8603dbc74b41ced0273
Waazux <yassinriahi03@gmail.com>
2026-06-16 21:27:25 +0200

    Fix link api admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "https://portfolio-production-aa49.up.railway.app/api";
 
 // 🔐 SÉCURITÉ : Récupérer le token du localStorage
 const token = localStorage.getItem("adminToken");
